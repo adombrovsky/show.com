8bedc11f5a74eeaa15aafad22041936a915056a6
farwless <dombrovsky.alex@gmail.com>
2013-12-14 23:30:10 +0200

    bugs

--- app/config.js
+++ app/config.js
@@ -46,7 +46,7 @@ exports.appConfig = {
         },
         mailer:{
             from: '',
-            host: '', // hostname
+            host: 'smtp.gmail.com', // hostname
             secureConnection: true, // use SSL
             port: 465, // port for secure SMTP
             transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
